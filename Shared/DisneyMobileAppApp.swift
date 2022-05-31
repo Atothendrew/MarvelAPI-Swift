be193026d81992e218a3da300f3127c86d52cf7a
axwilliamson <axwilliamson@godaddy.com>
2022-05-31 12:44:17 -0700

    Initial Commit

---
+++ Shared/DisneyMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyMobileAppApp.swift
+//  Shared
+//
+//  Created by Andrew Williamson on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct DisneyMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
